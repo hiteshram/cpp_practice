7ca32418a30f87f9cf0bc2ee863af6f7ca39ef5e
Hitesh Ram <hiteshram.kotha@gmail.com>
2020-08-10 18:09:54 -0600

    Input to cpp

---
+++ Input/cpp_io.cpp
@@ -0,0 +1,24 @@
+//Take a integer, float, string and display
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    float b;
+    char c[100];
+
+    cout<<"Enter an integer"<<endl;
+    cin>>a;
+    cout<<"Enter a float"<<endl;
+    cin>>b;
+    cout<<"Enter a string"<<endl;
+    cin>>c;
+
+    cout<<"Integer given is "<<a<<endl;
+    cout<<"Float given is "<<b<<endl;
+    cout<<"String given is "<<c<<endl;
+
+    return 0;
+}
